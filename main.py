64c160a17a1a72e6b92a486ea8d08d9310ce0694
NolanSpiekermeier10 <820382@mystma.org>
2022-09-20 09:34:13 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y): 
+    if x > y:
+        print(True)
+    else:
+        print(False)
+def lessThan(x,y):
+    if x < y:
+        print(True)
+    else:
+        print(False)
+def equalTo(x,y):
+    if x == y:
+        print(True)
+    else:
+        print(False)
+def greaterOrEqual(x,y):
+    if x >= y:
+        print (True)
+    else:
+        print(False)
+def lessOrEqual(x,y):
+    if x <= y:
+        print(True)
+    else:
+        print(False)
